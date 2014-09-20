7ca327bf1d303004eacc0778d1091637a8fedbe7
Dev96 <joel.witmer@gmx.ch>
2014-09-20 13:47:47 +0200

    comment

--- AppQuest/src/com/example/appquest/MainActivity.java
+++ AppQuest/src/com/example/appquest/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	//TEST
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
